4bb55ff00bae89458add5d245b39d4e27fbd2d6e
hamdimalihy <Hamdi99@>
2020-05-24 22:46:54 +0200

    second commit by Hamdi

--- Gestion_Personnel/src/metodebd.java
+++ Gestion_Personnel/src/metodebd.java
@@ -1,7 +1,10 @@
 import java.sql.Connection;
+import javax.swing.*;
+import java.sql.Date;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 
 public class metodebd {
 	
@@ -9,10 +12,10 @@ public class metodebd {
 	
 	// Méthode qui permet d'ajouter à la table Agent un candidat admis
 	
-	public void ajouter_agent(int id_agent ,String nom, String prenom, String adresse) {
+	public void ajouter_agent(String  id_agent ,String nom, String prenom, String adresse) {
 		try {
 			PreparedStatement ps=conn.prepareStatement("INSERT INTO agents (ID_AGENT, NOM, PRENOM,ADRESSE) VALUES (?, ?, ?,?)");
-				ps.setInt(1,id_agent);
+				ps.setString(1,id_agent);
 				ps.setString(2,nom);
 				ps.setString(3,prenom);
 				ps.setString(4,adresse);
@@ -22,6 +25,143 @@ public class metodebd {
 				}
 			catch(Exception e) {}
 			}
+	
+	//Méthode qui permet de supprimer un agent
+	public void supprimer_agent(String ID_AGENT) {
+		try {
+			PreparedStatement ps =conn.prepareStatement("DELETE FROM agents WHERE ID_AGENT LIKE ?");
+			ps.setString(1, ID_AGENT);
+			ps.executeUpdate();
+			ps.close();
+			conn.close();
+		}
+		catch(Exception e) {}
+	}
+	//Methode qui permet d'afficher les informations d'un agent
+	
+		public void afficher_agent(String mot) {
+			try {
+				PreparedStatement ps=conn.prepareStatement("SELECT * FROM agents WHERE ID_AGENT LIKE ?");
+				ps.setString(1,mot);
+				ResultSet rs=ps.executeQuery();
+				while(rs.next()) {
+					System.out.println("Les informations de cet agent sont :");
+				    System.out.println("ID_AGENT NOM  PRENOM ADRESSE ");
+					System.out.println(rs.getInt(1)+"         "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4));
+				}
+			}
+			catch(Exception e) { 
+			}
+		}
+	////Methode qui permet d'afficher tous les qualifications d'un agent durant sa carrière
+		public void afficher_les_qualification_agent(String id_agent) {
+			try {
+				PreparedStatement ps=conn.prepareStatement("SELECT * FROM agents,qualification ,agent_qualification WHERE agents.ID_AGENT =agent_qualification.ID_AGENT AND agent_qualification.ID_QUALIFICATION=qualification.ID_QUALIFICATION AND agents.ID_AGENT LIKE ?"); 
+				ps.setString(1,id_agent);
+				ResultSet rs=ps.executeQuery();
+				while(rs.next()) {
+					System.out.println("les informations de l'agent qui pocede ces qualifications");
+					System.out.println(rs.getInt(1)+"  " +rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4));
+					System.out.println("ce(s) qualification(s) sont :");
+					System.out.println(rs.getInt(5)+"   "+rs.getString(6));
+					
+				}
+			}
+			catch(Exception exp) {}
+		}
+	////Methode qui permet d'afficher tous les grades  d'un agent durant sa carrière
+		public void afficher_les_grade_agent(String ID_AGENT) {
+			try {
+				PreparedStatement ps=conn.prepareStatement("SELECT * FROM agents,grade,agent_grade WHERE agents.ID_AGENT=agent_grade.ID_AGENT AND grade.ID_GRADE=agent_grade.ID_GRADE AND agents.ID_AGENT LIKE ?");
+				ps.setString(1,ID_AGENT);
+				ResultSet rs=ps.executeQuery();
+			
+				while(rs.next()) {
+					System.out.println("les informations de l'agent qui pocede ces grades");
+					System.out.println(rs.getInt(1)+"  " +rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4));
+					System.out.println(" ces grades sont ID_GRADE LIBELE_GRADE DATE_OBTENTION_GRADE");
+					System.out.println(rs.getInt(5)+"    "+rs.getString(6)+"   "+rs.getDate(10));
+				}
+			}
+			
+			catch(Exception e) { 
+			}
+		}
+	////Methode qui permet d'afficher tous les postes d'un agent durant sa carrière
+		public void afficher_les_poste_agent(String ID_AGENT) {
+			try {
+				PreparedStatement ps=conn.prepareStatement("SELECT * FROM  agents,poste,decision WHERE agents.ID_AGENT = decision.ID_AGENT AND poste.ID_POSTE=decision.ID_POSTE AND decision.VISA=? AND agents.ID_AGENT LIKE ?");
+				ps.setString(1,"accepte");
+				ps.setString(2, ID_AGENT);
+				ResultSet rs=ps.executeQuery();
+				while(rs.next()) {
+				System.out.println("les informations de cet agent sont ");
+				System.out.println(rs.getString(1)+"   " +rs.getString(2)+"     "+rs.getString(3)+"   "+rs.getString(4));
+				System.out.println("les informations de ce(s) poste(s) sont ");
+				System.out.println(rs.getInt(5)+"    "+rs.getString(6));
+				}
+			}
+			catch(Exception e) {}
+		}
+		//Metode qui permet d'afficher toutes les unités d'affectation d'un agent durant sa carrière
+		public void afficher_les_unités_affectation_agent(String ID_AGENT) {
+			try {
+				PreparedStatement ps=conn.prepareStatement("SELECT * FROM affectation, agent_affectation WHERE affectation.ID_AFFECTATION=agent_affectation.ID_AFFECTATION AND agent_affectation.ID_AGENT LIKE ?");
+				ps.setString(1,ID_AGENT);
+				ResultSet rs=ps.executeQuery();
+				while(rs.next())
+				System.out.println(rs.getString(2));
+				}
+			
+			catch(Exception e) { 
+			}
+		}
+
+		
+		//Metode qui permet d'afficher tous les diplomes d'un agent
+		public void afficher_les_diplome_agent(String ID_AGENT) {
+			try {
+				PreparedStatement ps=conn.prepareStatement("SELECT * FROM diplome WHERE ID_AGENT LIKE ?");
+				ps.setString(1,ID_AGENT);
+				ResultSet rs=ps.executeQuery();
+				while(rs.next()) {
+				System.out.println("les diplomes qui sont obtenus par cet agent sont");
+				System.out.println("ID_DIPLOME LIBELE_DIPLOME DATE_OBTENTION_DIPLOME ");
+				System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getDate(3));
+				}
+			}
+			
+			catch(Exception e) { 
+			}
+		}
+		//Methode qui affiche tous les congés d'un agent
+		public void afficher_les_congé_agent(String id_agent) {
+			try {
+				PreparedStatement ps=conn.prepareStatement("SELECT * FROM congé WHERE ID_AGENT LIKE ?");
+				ps.setString(1, id_agent);
+				ResultSet rs=ps.executeQuery();
+				while(rs.next()) {
+					System.out.println("les congés qui a pris cet agent sont :");
+					System.out.println("ID_CONGE   LIBELE_CONGE  DATE_DEBUT_CONGE   DATE_FIN_CONGE");
+					System.out.println(rs.getInt(1)+"   "+rs.getString(2)+"    "+rs.getDate(4)+"    "+rs.getDate(5));
+				}
+			}
+			catch(Exception exp) {}
+		}
+	//Methode qui permet d'ajouter un diplome pour un agent
+	public void ajouter_diplome(int id_diplome,String libele_diplome,String simpleDateFormat ,int id_agent) {
+		try {
+			PreparedStatement ps=conn.prepareStatement("INSERT INTO diplome(ID_DIPLOME,LIBELE_DIPLOME,DATE_OBTENTION,ID_AGENT ) VALUES(?,?,?,?)");
+			ps.setInt(1, id_diplome);
+			ps.setString(2,libele_diplome);
+			ps.setString(3,simpleDateFormat);
+			ps.setInt(4,id_agent );
+			ps.executeUpdate();
+			ps.close();
+			conn.close();
+		}
+		catch(Exception e) {}
+	}
 	// Méthode qui permet de saisir  à la table decision la décision de recrutement d'un agent
 	
 	public void ajouter_decision(int id_decision ,int id_agent ,int id_poste, String visa) {
@@ -39,22 +179,7 @@ public class metodebd {
 		
 	}
 		
-	//Methode qui permet d'afficher les informations d'un agent
 	
-	public void afficher_agent(String mot) {
-		try {
-			PreparedStatement ps=conn.prepareStatement("SELECT * FROM agents WHERE NOM LIKE ?");
-			ps.setString(1,mot);
-			ResultSet rs=ps.executeQuery();
-			while(rs.next()) {
-				System.out.println("Les informations de cet agent sont :");
-			    System.out.println("ID_AGENT NOM  PRENOM ADRESSE ");
-				System.out.println(rs.getInt(1)+"         "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4));
-			}
-		}
-		catch(Exception e) { 
-		}
-	}
 	//Methode qui permet de saisir la fonction d'un agent dans une unité d'affectation
 	
 	public void ajouter_fonction(int id_agent_affectation ,int id_agent ,int id_affectation, String fonction) {
@@ -71,7 +196,7 @@ public class metodebd {
 		catch(Exception e) {}
 		
 	}
-	//Methode qui permet de suivre les postes d'un agent durant sa carrière
+	
 	
 	//Methode qui permet d'éditer à la demande d'un agent la décision de recrutement
 	
@@ -89,63 +214,25 @@ public class metodebd {
 		catch(Exception e) { 
 		}
 	}
-	////Methode qui permet d'afficher tous les postes d'un agent durant sa carrière
-	public void afficher_poste_agent(int ID_AGENT) {
+////Methode qui permet de modifier les informations d'un agent
+	public void modifier_agent(int NID_AGENT,String NNOM,String NPRENOM,String NADRESSE) {
 		try {
-			PreparedStatement ps=conn.prepareStatement("SELECT * FROM agents ,decision WHERE agents.ID_AGENT = decision.ID_AGENT AND agents.ID_AGENT LIKE ?");
-			ps.setInt(1,ID_AGENT);
-			ResultSet rs=ps.executeQuery();
-			while(rs.next())
-			System.out.println(rs.getInt(3));
-			}
-		
-		catch(Exception e) { 
-		}
+		PreparedStatement ps=conn.prepareStatement("UPDATE agents SET ID_AGENT=NID_AGENT,NOM=NNOM ,PRENOM=NPRENOM,ADRESSE=NADRESSE  WHERE ID_AGENT LIKE ?");
+		ps.setInt(1,NID_AGENT);
+		ps.setString(2, NNOM);
+		ps.setString(3,NPRENOM);
+		ps.setString(4, NADRESSE);
+		ps.executeUpdate();
+			
 	}
-////Methode qui permet d'afficher tous les grades  d'un agent durant sa carrière
-	public void afficher_grade_agent(int ID_AGENT) {
-		try {
-			PreparedStatement ps=conn.prepareStatement("SELECT * FROM agents,agent_grade WHERE agents.ID_AGENT=agent_grade.ID_AGENT AND agents.ID_AGENT LIKE ?");
-			ps.setInt(1,ID_AGENT);
-			ResultSet rs=ps.executeQuery();
-			while(rs.next())
-			System.out.println(rs.getInt(7)+" "+rs.getDate(8));
-			}
-		
-		catch(Exception e) { 
-		}
+		catch(Exception exp) {}
 	}
-//Metode qui permet d'afficher tous les diplomes d'un agent
-	public void afficher_diplome_agent(int ID_AGENT) {
-		try {
-			PreparedStatement ps=conn.prepareStatement("SELECT * FROM diplome WHERE ID_AGENT LIKE ?");
-			ps.setInt(1,ID_AGENT);
-			ResultSet rs=ps.executeQuery();
-			while(rs.next()) {
-			System.out.println("les diplomes qui sont obtenus par cet agent sont");
-			System.out.println("ID_DIPLOME LIBELE_DIPLOME DATE_OBTENTION_DIPLOME ");
-			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getDate(3));
-			}
-		}
-		
-		catch(Exception e) { 
-		}
-	}
+}
 	
-	//Metode qui permet d'afficher toutes les unités d'affectation d'un agent durant sa carrière
-	public void afficher_unité_affectation_agent(int ID_AGENT) {
-		try {
-			PreparedStatement ps=conn.prepareStatement("SELECT * FROM agents, agent_affectation WHERE agents.ID_AGENT=agent_affectation.ID_AGENT AND ID_AGENT LIKE ?");
-			ps.setInt(1,ID_AGENT);
-			ResultSet rs=ps.executeQuery();
-			while(rs.next())
-			System.out.println(rs.getInt(1));
-			}
-		
-		catch(Exception e) { 
-		}
-	}
-	}
+
+
+
+	
 	
 	
 	
--- Gestion_Personnel/src/test.java
+++ Gestion_Personnel/src/test.java
@@ -4,17 +4,19 @@ public class test {
 	public static void main(String[] args) {
 		metodebd m =new metodebd();
 		//m.ajouter_fonction(7, 2, 1, "qualiticien");
-		//m.ajouter_agent(5,"tibazi", "hatim", "settat");
+		//m.ajouter_agent("1","malihy", "hamdi", "casablanca");
 		//m.ajouter_decision(8 , 1 , 2,"différé");
 		//m.afficher_decision_recrutement(1);
-		//m.afficher_agent("malihy");
-		//m.afficher_poste_agent(1);
-		//m.afficher_grade_agent(1);
-		//m.afficher_diplome_agent(1);
-		//m.afficher_unité_affectation_agent(1);
-		//m.supprimer_agent();
-		//m.modifier();
-		//m.ajouterdiplome();
+		//m.afficher_agent("1");
+		//m.afficher_les_poste_agent(1);
+		//m.afficher_les_grade_agent("1");
+		//m.afficher_les_diplome_agent(1);
+		m.afficher_les_unités_affectation_agent("3");
+		//m.supprimer_agent("malihy");
+		//m.ajouter_diplome(3,"technicien_specialise","2021-01-02",1);
+		//m.afficher_les_qualification_agent("2");
+		//m.afficher_les_congé_agent(1);
+		//:m.modifier_agent(4, "guisser", "zineb", "casablanca");
 	}
 
 }
